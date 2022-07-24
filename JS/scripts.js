8dddf6089813bb4e1649303e47ec5e0e394cfc42
Patricia <patyamiga16@hotmail.com>
2022-07-24 16:04:12 -0300

    operaçao

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,8 +11,8 @@ class Calculator {
 
     //add  digit to calculator screen(adicionar dígito à tela da calculadora)
     addDigit(digit) {
-
-        //check digit to calculator  screen(dígito de verificação para a tela da calculadora)
+        console.log(digit);
+        //Check if number already has a dot(Verifica se o número já tem um ponto)
         if (digit === "." && this.currentOperationText.innerText.includes(".")) {
             return;
         }
@@ -23,6 +23,15 @@ class Calculator {
 
     //Process all constructor operations(processa todas as operações da calculadora)
     processOperation(operation) {
+        //Check if current value is empty(Verifique se o valor atual está vazio) 
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            //Change operation(Mudar operação)
+            if (this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
         //Get current and previous value(Obter valor atual e anterior)
         let operationValue
         const previous = +this.previousOperationText.innerText.split(" ")[0];
@@ -45,6 +54,18 @@ class Calculator {
                 operationValue = previous * current
                 this.updateScreen(operationValue, operation, current, previous);
                 break;
+            case "DEL":
+                this.processDelOperator();
+                break;
+            case "CE":
+                this.processClearCurrentOperator();
+                break;
+            case "C":
+                this.processClearOperator();
+                break;
+            case "=":
+                this.processEqualOperator();
+                break;
             default:
                 return;
         }
@@ -57,22 +78,56 @@ class Calculator {
         current = null,
         previous = null
     ) {
-
-        console.log(operationValue, operation, current, previous);
-
         if (operationValue === null) {
+            // Append number to current value(Anexa o número ao valor atual)
             this.currentOperationText.innerText += this.currentOperation;
         } else {
             //Check if value is zero, if it is just add current value
             //(Verifique se o valor é zero, se for apenas adicione o valor atual)
             if (previous === 0) {
-                operationValue = current
+                operationValue = current;
             }
 
             //Add current value to previous(Adicionar valor atual ao anterior)
             this.previousOperationText.innerText = `${operationValue} ${operation}`;
             this.currentOperationText.innerText = "";
         }
+    }
+
+    //Change math operation(Alterar operação matemática)
+    changeOperation(operation) {
+        const mathOperations = ["*", "-", "+", "/"];
+
+        if (mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previousOperationText.innerText =
+            this.previousOperationText.slice(0, -1) + operation;
+    }
+
+    //Delete a digit(Excluir um digito)
+    processDelOperator() {
+        this.currentOperationText.innerText =
+            this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    //Clear current operation(Limpar operação atual)
+    processClearOperator() {
+        this.currentOperationText.innerText = "";
+    }
+
+    //Clear all operations(Limpar todas as operações)
+    processClearOperator() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    //Process an operation(Processo da operação)
+    processEqualOperator() {
+        let operation = this.previousOperationText.innerText.split(" ")[1];
+
+        this.processOperation(operation);
     }
 }
 
